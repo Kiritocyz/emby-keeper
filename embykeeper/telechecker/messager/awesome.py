945eed40cabff53c4b030ee6e57e73884c1bdd71
Kiritocyz <yugiohcyz@gmail.com>
2025-01-11 01:42:51 +0800

    加了两个messager

---
+++ embykeeper/telechecker/messager/awesome.py
@@ -0,0 +1,6 @@
+from ._base import Messager
+
+class AwesomeMessager(Messager):
+    name = "Awesome"
+    chat_name = "awesomegogo"
+    default_messages = ["awesome.yaml * 1"]
---
+++ embykeeper/telechecker/messager/oracle.py
@@ -0,0 +1,6 @@
+from ._base import Messager
+
+class OracleMessager(Messager):
+    name = "Oracle"
+    chat_name = "nba8668"
+    default_messages = ["oracle.yaml * 1"]
