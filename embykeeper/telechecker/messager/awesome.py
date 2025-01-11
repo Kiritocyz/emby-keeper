baca675e7288bc7ea0087b01657729495fd2a737
Kiritocyz <Kiritocyz@users.noreply.github.com>
2025-01-12 02:53:59 +0800

    删除了两个文件，分别是awesome.py和oracle.py。

--- embykeeper/telechecker/messager/awesome.py
+++
@@ -1,6 +0,0 @@
-from ._base import Messager
-
-class AwesomeMessager(Messager):
-    name = "Awesome"
-    chat_name = "awesomegogo"
-    default_messages = ["awesome.yaml * 1"]
--- embykeeper/telechecker/messager/oracle.py
+++
@@ -1,6 +0,0 @@
-from ._base import Messager
-
-class OracleMessager(Messager):
-    name = "Oracle"
-    chat_name = "nba8668"
-    default_messages = ["oracle.yaml * 1"]
