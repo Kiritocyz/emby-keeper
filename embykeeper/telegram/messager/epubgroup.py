d11cdf047bbf4ed9143b4a214452e0832192cb2c
Kiritocyz <Kiritocyz@users.noreply.github.com>
2025-03-29 22:43:10 +0800

    add epubgroup messager

---
+++ embykeeper/telegram/messager/epubgroup.py
@@ -0,0 +1,9 @@
+from . import Messager
+
+__ignore__ = True
+
+
+class EpubgroupMessager(Messager):
+    name = "epub电子书库"
+    chat_name = "libhsulife"
+    default_messages = ["libhsulife.yaml * 8"]
