952ca552b6a780fae9d3e42a4b0ef04218aa7e1b
Kiritocyz <Kiritocyz@users.noreply.github.com>
2025-04-06 10:47:57 +0800

    fix bug in epubgroup.py

--- embykeeper/telegram/messager/epubgroup.py
+++ embykeeper/telegram/messager/epubgroup.py
@@ -6,4 +6,4 @@ __ignore__ = True
 class EpubgroupMessager(Messager):
     name = "epub电子书库"
     chat_name = "libhsulife"
-    default_messages = ["libhsulife.yaml * 8"]
+    default_messages = ["libhsulife.yaml"]
